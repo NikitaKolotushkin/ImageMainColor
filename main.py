ae268b912fc45250a151fbaa8fc8d5ee06b173e4
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-31 01:07:57 +0400

    Added algorithm for determining pixel color

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from PIL import Image
+
+
+img = Image.open("input_image.jpg")
+print(img.size)
+
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        print(img.getpixel((i, j)))
+        j += 1
+    i += 1
