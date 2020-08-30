f9588349ca86d9b30e084dd68863d0152b3d967a
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-08-31 01:43:10 +0400

    Added definition of the most frequent color

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from collections import Counter
 from PIL import Image
 
 
-img = Image.open("input_image.jpg")
+img = Image.open("img.webp")
 
 colors = []
 
 for i in range(img.size[0]):
     for j in range(img.size[1]):
-        #print(img.getpixel((i, j)))
         colors.append(img.getpixel((i, j)))
         j += 1
-    c = Counter(colors)
-    print(c)
-    break
+    c = list(Counter(colors).most_common(1))[0][0]
     i += 1
+print(c)
+
+new_img = Image.new("RGB", (100, 100), c).show()
